459754139711fda03546b76240c19f8c1e9f2cb1
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-06-17 16:54:26 +0200

    code ordered

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -9,8 +9,8 @@ const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
             <li
                 key={task.id}
                 className={
-                    `tasks__item${task.done && hideDone ?
-                        "tasks__item--hidden" : ""}`
+                    `tasks__item ${task.done && hideDone ? 
+                        " tasks__item--hidden" : ""}`
                 }>
                 <button
                     className="tasks__button tasks__button--toggleDone"
@@ -19,7 +19,7 @@ const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
                     {task.done ? "✔" : ""}
                 </button>
                 <span className={
-                    `tasks__content${task.done ? " tasks__content--done" : ""}`
+                    `tasks__content ${task.done ? " tasks__content--done" : ""}`
                 }>
                     {task.id} - {task.content}
                 </span>
