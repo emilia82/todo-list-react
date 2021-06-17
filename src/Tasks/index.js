b0099c7c1f7d9e22de5b238949a9f05dad459289
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-06-17 17:39:52 +0200

    code ordered

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -9,13 +9,9 @@ const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
             <li
                 key={task.id}
                 className={
-<<<<<<< HEAD
+
                     `tasks__item ${task.done && hideDone ? 
                         " tasks__item--hidden" : ""}`
-=======
-                    `tasks__item ${task.done && hideDone ?
-                        "tasks__item--hidden" : ""}`
->>>>>>> c7ff246711a819261f7618d70216eb6e3bcb829d
                 }>
                 <button
                     className="tasks__button tasks__button--toggleDone"
