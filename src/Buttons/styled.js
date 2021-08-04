c8ec4e3be948d75b13934c9b281f63412d17a97a
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-08-04 19:45:07 +0200

    buttons styled added

---
+++ src/Buttons/styled.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+export const Button = styled.button`
+    background: transparent;
+    border: none;
+    margin: 0 0 0 20px;
+    transition: 0.4s;
+    color: ${({ theme }) => theme.color.teal};
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        flex-basis: 100%;
+        margin: 10px;
+    }
+
+
+    &:hover {
+        filter: brightness(110%);
+    }
+
+    &:active {
+        filter: brightness(120%);
+    }
+
+
+    &:disabled {
+        color: ${({ theme }) => theme.color.silver};
+    }
+`;
