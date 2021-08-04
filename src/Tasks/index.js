71515cc0c3c7573f447dc0bcc5ae4ef96098bc88
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-08-04 19:48:46 +0200

    Tasks index.js modiefied

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,38 +1,34 @@
 import React from "react";
-import "./style.css";
+import { List, Item, Content, Button} from "./styled";
 
 
 
 const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
-    <ul className="tasks">
+    <List>
         {tasks.map(task => (
-            <li
+            <Item
                 key={task.id}
-                className={
-
-                    `tasks__item ${task.done && hideDone ? 
-                        " tasks__item--hidden" : ""}`
-                }>
-                <button
-                    className="tasks__button tasks__button--toggleDone"
+                hidden={task.done && hideDone}
+                >
+                <Button
+                    toggleDone
                     onClick={() => toggleTaskDone(task.id)}
                 >
                     {task.done ? "✔" : ""}
-                </button>
-                <span className={
-                    `tasks__content ${task.done ? " tasks__content--done" : ""}`
-                }>
-                    {task.id} - {task.content}
-                </span>
-                <button
-                    className="tasks__button tasks__button--remove"
+                </Button>
+                <Content done={task.done}
+                >
+                    {task.content}
+                </Content>
+                <Button
+                    remove
                     onClick={() => removeTask(task.id)}
                 >
                     🧺
-            </button>
-            </li>
+            </Button>
+            </Item>
         ))}
-    </ul>
+    </List>
 );
 
 export default Tasks;
