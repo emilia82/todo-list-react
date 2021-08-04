a5d1a48adc0abfddacb459cb93040c615fd94e29
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-08-04 19:47:07 +0200

    form styled added, index modified

---
+++ src/Form/styled.js
@@ -0,0 +1,37 @@
+import { filter } from "async";
+import styled from "styled-components";
+
+export const StyledForm = styled.form`
+    display: grid;
+    grid-template-columns: 1fr auto;
+    grid-gap: 20px;
+
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+            grid-template-columns: 1fr;
+    }
+`;
+
+export const Input = styled.input`
+    padding: 10px;
+    border: 2px solid ${({ theme }) => theme.color.alto};
+    
+    `; 
+    
+export const Button = styled.button`
+    padding: 10px;
+    background-color: ${({ theme }) => theme.color.teal};
+    color: ${({ theme }) => theme.color.white};
+    border: none;
+    transition: 0.4s;
+
+    &:hover {
+        filter: brigthtness(110%);
+        transform: scale(1.1);
+}
+
+    &:active {
+        filter: brightness(120%);
+}
+`;
+
