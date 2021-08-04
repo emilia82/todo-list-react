1035443079819c0c0603ed5fa875692d9ea67255
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-08-04 19:49:20 +0200

    Global Style created

---
+++ src/GlobalStyle.js
@@ -0,0 +1,22 @@
+import {createGlobalStyle} from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+    box-sizing: border-box;
+  }
+
+  *, ::after, ::before {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: "Montserrat", sans-serif;
+    font-size: 18px;
+    background: ${({ theme }) => theme.color.gallery};
+  }
+
+`;
+  
+  
+  
+  
