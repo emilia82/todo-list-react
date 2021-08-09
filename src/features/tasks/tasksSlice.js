fd9c7a405dbe4ccada4ee16cbdfcc0e04ceb22d1
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-08-09 12:46:12 +0200

    tasksSlice created from redux

---
+++ src/features/tasks/tasksSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const tasksSlice = createSlice({
+    name: `tasks`,
+    initialState: {
+        tasks: [],
+        hideDone: false,
+    },
+    reducers: {
+        addTask: ({ tasks }, { payload }) => {
+            tasks.push(payload);
+        },
+
+        toggleHideDone: state => {
+            state.hideDone = !state.hideDone;
+        },
+    },
+});
+
+
+
+export const { addTask, toggleHideDone } = tasksSlice.actions;
+export const selectTasks = state => state.tasks;
+export default tasksSlice.reducer;
+
+
+console.log(addTask({
+    content: "Nauczyć sie REduxa",
+    done: false,
+    id: 5,
+}
+));
