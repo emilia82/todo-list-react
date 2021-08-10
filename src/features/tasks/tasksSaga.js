845a6375ee524f38e4719d1a830fc02318d4964e
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-08-10 18:25:59 +0200

    tasksSaga updated

---
+++ src/features/tasks/tasksSaga.js
@@ -0,0 +1,23 @@
+import { takeEvery, takeLatest, call, put, select } from "redux-saga/effects";
+import { getExampleTasks } from "./getExampleTasks";
+import { fetchExampleTasks, selectTasks, setTasks } from "./tasksSlice";
+
+
+function* fetchExampleTasksHandler() {
+    try {
+        const exampleTasks = yield call(getExampleTasks);
+        yield put(setTasks(exampleTasks));
+    } catch (error) {
+        yield call(alert, "Coś poszło nie tak!");
+    }
+}
+function* saveTasksInLocalStorageHandler() {
+    const tasks = yield select(selectTasks);
+    yield call(saveTasksInLocalStorageHandler, tasks);
+}
+
+export function* watchFetchExampleTasks() {
+    yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
+    yield takeEvery("*", saveTasksInLocalStorageHandler);
+
+}
