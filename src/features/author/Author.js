20f7ecd787cf51d9e510dc161ec65892cb9447f7
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-08-12 16:15:23 +0200

    author page created

---
+++ src/features/author/Author.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Container from "../../common/Container";
+import Header from "../../common/Header";
+import Section from "../../common/Section";
+
+export default () => (
+
+    <Container>
+        <Header title="About Author" />
+        <Section
+            title="Emilia Warakomska"
+            body={
+                <>
+                    <p>
+                        <strong>Mam na imię Emilia i uwielbiam się uczyć.</strong> Moją ogromną pasją jest zdrowy styl życia,
+                        trenuję od 16 lat, a przez 10 lat byłam zafascynowana sportami sylwetkowymi, dlatego
+                        przez 5 lat trenowałam wyczynowo ten sport (z elementami gimnastyki). Niestety doznałam 2 
+                        poważnych kontuzji kolan.
+                    </p>
+                    <p>
+                        Kocham wyzwania, <em>nawet nie wiecie jak bardzo.</em> <strong>Mam charakter Wojownika,</strong> więc nie ma dla mnie rzeczy niemożliwych.
+                    </p>
+                </>
+
+            }
+        />
+    </Container>
+);
