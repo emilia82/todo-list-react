67423b26a395f03daf157b342f56ac1a734380a5
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-08-12 16:13:17 +0200

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,36 @@
 import React from "react";
-import Tasks from "./features/tasks/Tasks";
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import Author from "./features/author/Author";
+import { StyledNavLink } from "./styled";
+import { 
+    HashRouter, 
+    Link, 
+    Switch, 
+    Route, 
+    Redirect 
+} from "react-router-dom";
+import { toAuthor, toTask, toTasks } from "./routes";
+import Navigation from "./Navigation";
 
-export default () => <Tasks />;
+
+export default () => (
+    <HashRouter>
+        <Navigation />
+
+            <Switch>
+                <Route path={toTask()}>
+                    <TaskPage />
+                </Route>
+                <Route path={toTasks()}>
+                    <TasksPage />
+                </Route>
+                <Route path={toAuthor()}>
+                    <Author />
+                </Route>
+                <Route>
+                    <Redirect to={toTasks()} />
+                </Route>
+            </Switch>
+</HashRouter>
+);
