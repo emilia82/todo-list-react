35f38edeec6e68dade766acb5adb30c70af28987
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-08-15 16:20:06 +0200

    Inpput imported

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Input } from "../../Input";
+import Input from "../../Input";
 import searchQueryParamName from "../searchQueryParamName";
 import { useQueryParameter, useReplaceQueryParameter } from "../queryParameters";
 import { Wrapper } from "./styled";
