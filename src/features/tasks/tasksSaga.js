1fcce5e23a2d37a2284b36d4d1a43d52a1a310cb
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-08-24 17:28:13 +0200

    function corrected

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,13 +1,13 @@
 import { takeEvery, takeLatest, call, put, delay, select } from "redux-saga/effects";
 import { getExampleTasks } from "./getExampleTasks";
-import { fetchExampleTasks, selectTasks, setTasks} from "./tasksSlice";
+import { fetchExampleTasks, selectTasks, fetchExampleTasksSucces, fetchExampleTasksError} from "./tasksSlice";
 import { saveTasksInLocalStorage } from "../tasks/tasksLocalStorage";
 
 function* fetchExampleTasksHandler() {
     try {
         yield delay(1000);
         const exampleTasks = yield call(getExampleTasks);
-        yield put(setTasks(exampleTasks));
+        yield put(fetchExampleTasksSucces(exampleTasks));
     } catch (error) {
 
         yield call(alert, "Coś poszło nie tak!");
